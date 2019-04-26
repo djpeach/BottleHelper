bb3975ae5c4fdd78f8807faf136eb8bda3b43c73
Daniel Peach <dpeaches96@gmail.com>
2019-04-26 18:49:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from bottle import route, run, template, request, post, get, error
 def index():
 	return "<h1>Hello, world</h1><br><p>This is my home page!</p>"
 
-# Route with Wildcard
+# Route with URL Parameter
 @route('/hello/<name>')
 def index(name):
 	return template('index', name=name)
@@ -35,22 +35,22 @@ def show_article(id=None):
 def server_static(filename):
 	return static_file(filename, root="./static")
 
-# Querying..idk much about this
-@route('/querytest')
+# Querying (example url: `/search?firstName=Daniel&lastName=Peach` )
+@route('/search')
 def querytest():
-	p1 = request.query.p1 # hello
-	p2 = request.query.p2 # world
-	return "{}, {}".format(p1.capitalize(), p2)
+	first_name = request.query.firstName
+	last_name = request.query.lastName
+	return "{}, {}".format(first_name, last_name)
 
-# Speical '@get' route 
-@get('/testform')
+# GET only route
+@get('/form')
 def form():
 	return '''<form method="POST" action="/test">
 				<input type="text" name="name1">
 				<input type="submit" value="submit">
 			  </form>'''
-# Special '@post' route
-@post('/test')
+# POST only route
+@post('/submit form')
 def submit():
 	username = request.forms.get('name1')
 	return "<h4>Hi there, {}</h4>".format(username)
@@ -60,11 +60,14 @@ def submit():
 def error404(error):
 	return "You have exerienced a 404 error"
 
-# Using json data...idk much about this
-@route('/jsondata')
-def jsondata():
-	return {"name": "Jason", "List": [1,2,3,4,5]}
+# Using json data (usefule for RESTful API's)
+@route('/api')
+def api():
+	return {
+		"name": "Jason", 
+		"List": [1,2,3,4,5]
+	}
 
-# Run the server
+# Run the server (serve on your local host, on port 8080, set debug to true during development, and reloader to True to update as you make changes)
 run(host='localhost', port=8080, debug=True, reloader=True)
 
